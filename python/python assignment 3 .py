5ab547242af5f1e44bfaa8566d9f505e48ee5f40
Tanish2005 <148605647+Tanish2005@users.noreply.github.com>
2025-03-29 20:09:25 +0530

    python assignment 3 .py

--- python/python assignment 3 .py
+++ python/python assignment 3 .py
@@ -1,16 +1,21 @@
 import matplotlib.pyplot as plt
 
-# Data for plotting
 months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+
 sales = [200, 300, 400, 350, 500, 600, 650 , 750 ,  900 ,1000 , 1300 , 2000] 
-# Create a line plot
+
+
 plt.plot(months, sales, marker='o', linestyle='-', color='b', label='Sales')
 
-# Add labels and title
+
 plt.xlabel('Months')
+
 plt.ylabel('Sales ($)')
+
 plt.title('Monthly Sales Data')
+
 plt.legend()
 
-# Show the plot
+
+
 plt.show()
